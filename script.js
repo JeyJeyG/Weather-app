9194a529bb7250e6d9baae0d1f14bac26c5cf0de
jey <jeyjeygraham@gmail.com>
2023-08-09 10:02:31 +0000

    Js build up

---
+++ script.js
@@ -0,0 +1,5 @@
+const cityInput = document.getElementById("cityInput");
+const searchButton = document.getElementById("searchButton");
+const weatherinfo = document.getElementById("weatherInfo");
+
+const API_KEY = "YOUR_API_KEY" // Replace 
